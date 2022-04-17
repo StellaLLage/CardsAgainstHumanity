3b2f708ba3e90246816a6ee86229b759bfa96bde
StellaLLage <stellallage@hotmail.com>
2022-04-17 01:05:25 -0300

    Photon launcher scene connection made

---
+++ Assets/Scripts/RoomSystem/Launcher.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    void Awake()
+    {
+        //Isso faz com que quando LoadLevel for chamado no Master Client, todos os outros clients da room vão atualizar automaticamente
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    private void Start()
+    {
+        Connect();
+    }
+
+    private void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    private void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    /// <summary>
+    /// Start the connection process.
+    /// - If already connected, we attempt joining a random room
+    /// - if not yet connected, Connect this application instance to Photon Cloud Network
+    /// </summary>
+    public void Connect()
+    {
+        //Checa se tá conectado ou não, se tiver dá join, senão faz a conexão do servidor
+        if (PhotonNetwork.IsConnected)
+        {
+            //PhotonNetwork.JoinOrCreateRoom();
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            //Conecta no servidor
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+}
